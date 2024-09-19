9b665da01fc9246266a9fd345df1f6bc6382991e
9names <60134748+9names@users.noreply.github.com>
2024-09-19 16:59:41 +1000

    Update args to be closer to picotool

--- picotool-cli/src/main.rs
+++ picotool-cli/src/main.rs
@@ -1,25 +1,40 @@
-use clap::Parser;
+use camino::Utf8PathBuf;
+use clap::{Args, Parser};
 use picotool::{picotool_reset::reset_usb_device, PicoTool};
-use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(short)]
-    reset: bool,
-    /// UF2 file to flash
-    #[arg(value_name = "FILE")]
-    target_file: Option<PathBuf>,
+    /// Force-reset a USB device that supports the "Reset to Bootsel" instruction
+    #[arg(short, global = true)]
+    force_reset: bool,
+    #[command(subcommand)]
+    cmd: Subcommand,
 }
+
+#[derive(Debug, Args)]
+struct WriteArgs {
+    /// UF2 file to flash
+    target_file: Utf8PathBuf,
+}
+
+#[derive(clap::Subcommand)]
+enum Subcommand {
+    /// Load data into flash on your RP microcontroller
+    Load(WriteArgs),
+}
+
 fn main() {
     let cli = Cli::parse();
 
-    if cli.reset {
+    if cli.force_reset {
         reset_usb_device();
         return;
     }
 
     let mut tool = PicoTool::new();
-    if let Some(file) = &cli.target_file {
-        tool.flash_uf2(file);
+    match cli.cmd {
+        Subcommand::Load(write_args) => {
+            tool.flash_uf2(write_args.target_file.as_std_path());
+        }
     }
 }
